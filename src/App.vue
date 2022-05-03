a7886893b124a1acb46a705171da108d2f9cd8a4
정수범 <teller75@naver.com>
2022-05-03 18:00:55 +0900

    data binding

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4 :style="h4style">{{products[1]}}</h4>
+    <p>{{price1}} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>{{price2}} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return {
+      price1 : 60,
+      price2 : 70,
+      h4style : "color : blue",
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
