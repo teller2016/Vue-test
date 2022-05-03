4abb294136e9ea15e5c3ab9e0827c9fe31ac59f3
정수범 <teller75@naver.com>
2022-05-03 19:02:22 +0900

    modal made

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
 
+  <div class="black-bg" v-if="modalState == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="modalState=false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, i) in menues" :key="i">{{menu}}</a>
 
@@ -12,16 +20,19 @@
     <span>신고수 : {{reports[i]}}</span>
   </div> -->
   <div>
-    <h4>{{products[0]}}</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modalState=true">{{products[0]}}</h4>
     <p>50 만원</p>
     <button @click="increase(0)">허위매물신고</button> <span>신고수 : {{reports[0]}}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4 :style="h4style">{{products[1]}}</h4>
     <p>{{price1}} 만원</p>
     <button @click="increase(1)">허위매물신고</button> <span>신고수 : {{reports[1]}}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4>{{products[2]}}</h4>
     <p>{{price2}} 만원</p>
     <button @click="increase(2)">허위매물신고</button> <span>신고수 : {{reports[2]}}</span>
@@ -34,6 +45,10 @@ export default {
   name: 'App',
   data(){
     return {
+
+      modalState : false,
+
+
       reports : [0,0,0],
 
       menues : ['Home', 'Shop', 'About'],
@@ -56,6 +71,28 @@ export default {
 </script>
 
 <style>
+
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;  
+}
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -73,6 +110,11 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img{
+  width:100%;
+  margin-top: 40px;
 }
 
 </style>
